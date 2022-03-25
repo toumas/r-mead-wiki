547cbb0b82cac8ddeadffa703015da1fbd8dbdad
Tuomas Ukkola <dm@ukko.la>
2022-03-25 20:04:23 +0200

    Add heading components

--- components/map.tsx
+++ components/map.tsx
@@ -1,7 +1,14 @@
 import { Anchor } from "./Anchor";
 import { HorizontalRule } from "./Hr";
+import { H1, H2, H3, H4, H5, H6 } from "./Heading";
 
 export const components = {
   a: Anchor,
   hr: HorizontalRule,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
 };
