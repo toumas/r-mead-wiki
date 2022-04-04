b91156d6e7af6ac33f0d0e75e277311b18f748ed
Tuomas Ukkola <dm@ukko.la>
2022-04-04 14:06:20 +0300

    Add List component

---
+++ components/List.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+import tw, { styled, TwStyle } from "twin.macro";
+
+export interface ListProps {
+  children: ReactNode;
+  padding?: TwStyle;
+  border?: TwStyle;
+}
+
+type StyleProps = Exclude<ListProps, "children">;
+
+export const StyledList = styled.div`
+  & > * {
+    ${({ padding }: StyleProps) => padding ?? tw`p-4`}
+    ${({ border }: StyleProps) =>
+      border ?? tw`border-b-2 border-brand-bright-black`}
+  }
+  & > *:last-child {
+    border-bottom: none;
+  }
+`;
+
+export function List({ children, padding, border, ...props }: ListProps) {
+  return (
+    <StyledList padding={padding} border={border} {...props}>
+      {children}
+    </StyledList>
+  );
+}
+
+// ${({ padding }: { padding: any }) => padding}
