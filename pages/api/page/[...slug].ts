aa03f44b5b294b26defb1570b93e65133e7d337d
Tuomas Ukkola <dm@ukko.la>
2022-04-07 12:18:38 +0300

    Revert "Write page data to .next to use on server"
    This reverts commit d88286e140639f209314b2ff7f512f602068666c.

--- pages/api/page/[...slug].ts
+++ pages/api/page/[...slug].ts
@@ -8,20 +8,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const slugObject = slugToObject(req.query.slug);
-  try {
-    const basePath = process.cwd();
-    const file = await fs.readFile(
-      path.join(
-        basePath,
-        "../../",
-        `${process.env.NODE_ENV === "development" ? "compiledpages" : ""}/${
-          slugObject.path
-        }`
-      )
-    );
-    res.status(200).json(JSON.parse(file.toString()));
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({});
-  }
+  const file = await fs.readFile(
+    path.join(
+      process.cwd(),
+      `${process.env.NODE_ENV === "development" ? "/public" : ""}/_pages/${
+        slugObject.path
+      }`
+    )
+  );
+  res.status(200).json(JSON.parse(file.toString()));
 }
