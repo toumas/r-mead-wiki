47d2b66e98e306d0c6872ba645bbf3b240c11549
Tuomas Ukkola <dm@ukko.la>
2022-04-07 15:06:53 +0300

    Store compiled pages in Supabase storage

--- pages/api/page/[...slug].ts
+++ pages/api/page/[...slug].ts
@@ -1,20 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import fs from "fs/promises";
 import { slugToObject } from "../../../utils";
+import { supabase } from "../../../supabaseClient";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const slugObject = slugToObject(req.query.slug);
-  const file = await fs.readFile(
-    path.join(
-      process.cwd(),
-      `${process.env.NODE_ENV === "development" ? "/public" : ""}/_pages/${
-        slugObject.path
-      }`
-    )
-  );
-  res.status(200).json(JSON.parse(file.toString()));
+  try {
+    const { data, error } = await supabase.storage
+      .from("compiled-pages")
+      .download(slugObject.path);
+    if (error) {
+      throw new Error(error.message);
+    }
+    if (data === null) {
+      res.status(404);
+    } else {
+      const text = await data?.text();
+      res.status(200).json(JSON.parse(text));
+    }
+  } catch (catchedError) {
+    throw catchedError;
+  }
 }
