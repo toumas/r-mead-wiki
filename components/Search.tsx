12222a25b84a0f2b0b70c4fa2dce72ce3b4aac9b
Tuomas Ukkola <dm@ukko.la>
2022-04-12 15:18:24 +0300

    Connect search pagination to URL

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,39 @@
 import algoliasearch from "algoliasearch/lite";
 import { Configure, InstantSearch } from "react-instantsearch-dom";
+import QueryString from "qs";
+import { MultipleQueriesQuery } from "@algolia/client-search";
 import Hits from "./Hits";
 import SearchBox from "./SearchBox";
 
-const searchClient = algoliasearch(
+const algoliaClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string,
+  {}
 );
+
+const searchClient = {
+  ...algoliaClient,
+  search(requests: MultipleQueriesQuery[]) {
+    const searchParams = QueryString.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    const nextRequests = requests.map((r) => {
+      if (searchParams.page) {
+        return {
+          ...r,
+          params: {
+            ...r.params,
+            page: parseInt((searchParams.page ?? "0") as string) - 1,
+          },
+        };
+      }
+      return r;
+    });
+
+    return algoliaClient.search(nextRequests);
+  },
+};
 
 export function Search() {
   return (
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,5 +1,7 @@
 import VisuallyHidden from "@reach/visually-hidden";
+import { isNil } from "lodash";
 import { useRouter } from "next/router";
+import QueryString from "qs";
 import {
   useState,
   ChangeEvent,
@@ -12,6 +14,7 @@ import { SearchBoxProvided } from "react-instantsearch-core";
 import { connectSearchBox } from "react-instantsearch-dom";
 import tw, { styled } from "twin.macro";
 import { useDebounce } from "usehooks-ts";
+import useNavigation from "../hooks/usePush";
 import { Stack } from "./Stack/Stack";
 
 const StyledStack = styled(Stack)`
@@ -20,20 +23,62 @@ const StyledStack = styled(Stack)`
 
 function SearchBox({ refine }: SearchBoxProvided) {
   const router = useRouter();
+  const { push, replace } = useNavigation();
   const [value, setValue] = useState<string>("");
+
   const debouncedValue = useDebounce<string>(value, 250);
   const [dirty, setDirty] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    refine(event.currentTarget.value);
-    setValue(event.target.value);
-    setDirty(true);
-  };
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      refine(event.currentTarget.value);
+      setValue(event.target.value);
+      setDirty(true);
+    },
+    [refine]
+  );
 
-  const handleSubmit = useCallback((e: FormEvent) => {
-    e.preventDefault();
-  }, []);
+  const updateUrl = useCallback(() => {
+    const searchParams = QueryString.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    const nextSearchParams = QueryString.stringify(
+      { ...searchParams, query: value, page: 1 },
+      { addQueryPrefix: true }
+    );
+
+    push(`${window.location.origin}/${nextSearchParams}`);
+  }, [push, value]);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+      if (inputRef.current) {
+        updateUrl();
+      }
+    },
+    [updateUrl]
+  );
+
+  useEffect(() => {
+    if (value.length <= 0) {
+      return;
+    }
+
+    const searchParams = QueryString.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    if (isNil(searchParams.page)) {
+      return;
+    }
+
+    if (searchParams.query !== value) {
+      updateUrl();
+    }
+  }, [updateUrl, value]);
 
   useEffect(() => {
     if (inputRef.current) {
@@ -43,15 +88,31 @@ function SearchBox({ refine }: SearchBoxProvided) {
 
   useEffect(() => {
     if (debouncedValue.length > 0 || dirty) {
-      router.push(router.asPath.replace(/(?<=query=).*/, debouncedValue));
+      const searchParams = QueryString.parse(window.location.search, {
+        ignoreQueryPrefix: true,
+      });
+
+      const nextSearchParams = QueryString.stringify(
+        { ...searchParams, query: debouncedValue },
+        { addQueryPrefix: true }
+      );
+
+      if (
+        typeof searchParams.query === "string" &&
+        searchParams.query.length > 0
+      ) {
+        push(`${window.location.origin}/${nextSearchParams}`);
+      } else {
+        replace(`${window.location.origin}/${nextSearchParams}`);
+      }
     }
-  }, [debouncedValue, dirty]);
+  }, [debouncedValue, dirty, push, replace]);
 
   useEffect(() => {
     if (router.query.query?.[0]) {
       refine(router.query.query);
     }
-  }, [refine, router.query.query]);
+  }, [refine, router.query]);
 
   return (
     <form onSubmit={handleSubmit}>
